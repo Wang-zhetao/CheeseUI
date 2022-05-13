5d6f91a7184930516498f02fbba4372668829de4
Wang-zhetao <wzt15857662906@163.com>
2022-05-13 15:06:23 +0800

    Topanv 组件封装好了

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,20 @@
 <template>
-    <div>
-        <div class="topnav">
-            <div class="logo">LOGO</div>
-            <ul class="menu">
-                <li>菜单1</li>
-                <li>菜单2</li>
-            </ul>
-        </div>
-        <div class="banner">
-            <h1>芝士UI</h1>
-            <h2>一个很厉害的 UI 框架</h2>
-            <p class="actions">
-                <a href="javascript()">Github</a>
-                <a href="javascript()">开始</a>
-            </p>
-        </div>
+  <div>
+    <Topnav />
+    <div class="banner">
+      <h1>芝士UI</h1>
+      <h2>一个很厉害的 UI 框架</h2>
+      <p class="actions">
+        <a href="javascript()">Github</a>
+        <a href="javascript()">开始</a>
+      </p>
     </div>
+  </div>
 </template>
 
-<script>
-    export default {
-        
-    }
+<script lang="ts">
+import Topnav from "../components/Topnav.vue";
+export default {
+  components: { Topnav },
+};
 </script>
-
-<style lang="scss" scoped>
-.topnav{
-    background: pink;
-    display: flex;
-    padding: 16px;
-    >.logo{
-        max-width: 6em;
-        margin-right: auto;
-    }
-    > .menu{
-        display: flex;
-        white-space: nowrap;
-        flex-wrap: nowrap;
-        >li{
-            margin: 0 1em;
-        }
-    }
-}
-</style>
