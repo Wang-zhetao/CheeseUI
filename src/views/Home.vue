8abaa58f2727b518c286ace4dc2f11b44ecdd5de
Wang-zhetao <wzt15857662906@163.com>
2022-05-13 15:18:11 +0800

    初始化 asidea 的 html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>芝士UI</h1>
       <h2>一个很厉害的 UI 框架</h2>
       <p class="actions">
-        <a href="javascript()">Github</a>
-        <a href="javascript()">开始</a>
+        <a href="https://github.con">Github</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
