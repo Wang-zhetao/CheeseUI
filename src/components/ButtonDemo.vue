3263f9556ba90367d1be9bb40db08599ae5f8ac4
Wang-zhetao <wzt15857662906@163.com>
2022-05-14 12:37:35 +0800

    add theme 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,19 +1,20 @@
 <template>
-  <Button @click="onClick" 
-          @focus="onClick"
-          @mouseover="onClick">你好</Button>
+  <Button>你好</Button>
+  <Button theme="button">你好</Button>
+  <Button theme="link">你好</Button>
+  <Button theme="text">你好</Button>
 </template>
 
 <script lang="ts">
-import Button from '../lib/Button.vue'
+import Button from "../lib/Button.vue";
 export default {
-  components:{Button},
-  setup(){
-    const onClick = ()=>{
-      console.log('hi')
-    }
-    return {onClick}
-  }
+  components: { Button },
+  setup() {
+    const onClick = () => {
+      console.log("hi");
+    };
+    return { onClick };
+  },
 };
 </script>
 
