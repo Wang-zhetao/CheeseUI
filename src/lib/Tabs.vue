d17490cc11be98abb8fb382a1b6a7c35bde86583
Wang-zhetao <wzt15857662906@163.com>
2022-05-19 16:27:28 +0800

    indicator css 1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,11 +1,14 @@
 <template>
-<div class="i-tabs">
-  <div class="i-tabs-nav">
-    <div class="i-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
-  </div>
-  <div class="i-tabs-content">
-    <component class="i-tabs-content-item" :class="{selected: c.props.title === selected}" v-for="c in defaults" :is="c" />
-  </div>
+  <div class="i-tabs">
+    <div class="i-tabs-nav">
+      <div class="i-tabs-nav-item" v-for="(t, index) in titles" @click="select(t)" :class="{ selected: t === selected }"
+        :key="index">{{ t }}</div>
+      <div class="i-tabs-nav-indicator"></div>
+    </div>
+    <div class="i-tabs-content">
+      <component class="i-tabs-content-item" :class="{ selected: c.props.title === selected }" v-for="c in defaults"
+        :is="c" />
+    </div>
   </div>
 </template>
 
@@ -13,11 +16,11 @@
 import Tab from './Tab.vue'
 import { computed } from 'vue'
 export default {
-    props:{
-        selected:{
-            type:String
-        }
-    },
+  props: {
+    selected: {
+      type: String
+    }
+  },
   setup(props, context) {
     const defaults = context.slots.default()
     defaults.forEach((tag) => {
@@ -51,28 +54,44 @@ export default {
 $blue: #40a9ff;
 $color: #333;
 $border-color: #d9d9d9;
+
 .i-tabs {
   &-nav {
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
+
     &-item {
       padding: 8px 0;
       margin: 0 16px;
       cursor: pointer;
+
       &:first-child {
         margin-left: 0;
       }
+
       &.selected {
         color: $blue;
       }
     }
+
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+    }
   }
+
   &-content {
     padding: 8px 0;
 
-     &-item {
+    &-item {
       display: none;
+
       &.selected {
         display: block;
       }
