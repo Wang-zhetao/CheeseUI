b0ebe18b8b9f42947d27e2cb5854cb065c5a1e9b
Wang-zhetao <wzt15857662906@163.com>
2022-05-19 22:56:12 +0800

    features 样式

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -6,8 +6,12 @@
       </svg>
     </div>
     <ul class="menu">
-      <li>菜单1</li>
-      <li>菜单2</li>
+      <li>
+        <router-link to="/doc">文档</router-link>
+      </li>
+      <li>
+        <a href="https://githu.con">GitHub</a>
+      </li>
     </ul>
     <span class="toggleAside" @click="toggleMenu"></span>
   </div>
