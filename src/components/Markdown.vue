21922ae6cdf3e9d227ca90c80e31fe094751f480
Wang-zhetao <wzt15857662906@163.com>
2022-05-23 10:27:19 +0800

    事不过三：优化markdown

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -1,10 +1,12 @@
 <template>
-  <article class="markdown-body" v-html="content">
-  </article>
+<article class="markdown-body" v-html="content">
+</article>
 </template>
 
 <script lang="ts">
-import { ref } from 'vue'
+import {
+  ref
+} from 'vue'
 export default {
   props: {
     path: {
@@ -13,7 +15,7 @@ export default {
     }
   },
   setup(props) {
-    const content = ref<string>(null)
+    const content = ref < string > (null)
     import(props.path).then(result => {
       content.value = result.default
     })
@@ -23,6 +25,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-</style>
