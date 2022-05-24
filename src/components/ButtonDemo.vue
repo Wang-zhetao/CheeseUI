dff09a61f8aa26f113cf8735fbdd4470fc2418b5
Wang-zhetao <wzt15857662906@163.com>
2022-05-24 17:05:32 +0800

    改造 Tabs Demo

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,12 +1,10 @@
 <template>
-<div>
   <h1>Button示例</h1>
   <Demo :component="Button1Demo" />
   <Demo :component="Button2Demo" />
   <Demo :component="Button3Demo" />
   <Demo :component="Button4Demo" />
   <Demo :component="Button5Demo" />
-  </div>
 </template>
 
 <script lang="ts">
